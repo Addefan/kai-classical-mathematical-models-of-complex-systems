7a0ec214131c972ab81899393f8b4f8e7d46bec2
Vitaly Zorin <addefan@mail.ru>
2025-09-29 17:06:42 +0300

    feat: parsing influenza statistics into csv

---
+++ src/parse_influenza.py
@@ -0,0 +1,81 @@
+import csv
+import re
+import time
+
+from requests import Session
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
+BASE_URL = "https://www.influenza.spb.ru/surveillance/flu-bulletin"
+
+retry_strategy = Retry(
+    total=5,
+    status_forcelist=[504],
+    allowed_methods=["GET"]
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+session = Session()
+session.mount("http://", adapter)
+session.mount("https://", adapter)
+
+
+def extract_incidence_rate(html: str):
+    incidence = re.search(r"составив\s(\d+[,.]\d)\sна\s10\s000\sнаселения", html)
+    if incidence is None:
+        return None
+
+    return float(incidence.group(1).replace(",", "."))
+
+
+def fetch_bulletin(year: int, week: int):
+    params = {
+        "year": str(year),
+        "week": f"{week:02d}"
+    }
+
+    response = session.get(BASE_URL, params=params)
+    response.raise_for_status()
+    time.sleep(0.5)
+
+    incidence = extract_incidence_rate(response.text)
+    return incidence
+
+
+def _get_year_week_pairs(start_year: int, start_week: int, end_year: int, end_week: int):
+    years_weeks = []
+
+    for week in range(start_week, 53 + 1):
+        years_weeks.append((start_year, week))
+
+    for year in range(start_year + 1, end_year):
+        for week in range(1, 53 + 1):
+            years_weeks.append((year, week))
+
+    for week in range(1, end_week + 1):
+        years_weeks.append((end_year, week))
+
+    return years_weeks
+
+
+def fetch_statistics(start_year: int, start_week: int, end_year: int, end_week: int, output_csv="out.csv"):
+    with open(output_csv, "w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=["year", "week", "incidence"])
+        writer.writeheader()
+
+        for year, week in _get_year_week_pairs(start_year, start_week, end_year, end_week):
+            incidence = fetch_bulletin(year, week)
+            if incidence is None:
+                print(f"Данных за {week} неделю {year} года нет.")
+                continue
+
+            row = {
+                "year": year,
+                "week": week,
+                "incidence": incidence,
+            }
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    start, end = (2020, 17), (2025, 38)
+    fetch_statistics(*start, *end, output_csv="statistics.csv")
